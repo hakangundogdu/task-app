17ed7f25cab78dae1e525e33a90ff762bb8109a2
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2021-11-20 16:15:43 +0300

    Task manager app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card">
+          <header className="header">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <h1 className="title">Task Manager</h1>
+            <p className="text-gray">{new Date().toDateString()}</p>
+          </header>
+          <div className="tool-bar">
+            <p>5 tasks</p>
+            <div className="filters">
+              <div className="filter is-active">All</div>
+              <div className="filter">Active</div>
+              <div className="filter">Completed</div>
+            </div>
+          </div>
+          <input
+            className="add-task"
+            placeholder="Add a New Task..."
+            // value={enteredDepartment}
+            // onChange={departmentChangeHandler}
+          />
+
+          <div>Task List</div>
+        </div>
+      </div>
     </div>
   );
 }
